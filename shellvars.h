554dc9655d4bc7ca0254517600a7abb13b078693
Peter Imade <84706930+Pete5145@users.noreply.github.com>
2022-08-12 10:06:24 +0100

    Update shellvars.h

--- shellvars.h
+++ shellvars.h
@@ -1,12 +1,12 @@
 #ifndef SHELLVARH
 #define SHELLVARH
-
 /**
  * struct ShellVar - shellvar list node
  * @name: name of variable
  * @val: value of variable
  * @next: next variable in the list
  */
+
 typedef struct ShellVar
 {
 	char *name;
